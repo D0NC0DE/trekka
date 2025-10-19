b7cdf478af1d727c2c7f6ea4eb1aed658b6eb8b5
Winner Olusola <OLUSOLAWINNER1@GMAIL.COM>
2025-10-19 18:59:48 +0100

    Revert "🌍 feat: init prisma (#1)" (#2)
    This reverts commit fa5d88d43562b4275619e5d663d768dae4a092fc.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { WalletsModule } from './wallets/wallets.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, WalletsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
