472fa2d1e83efa39c09478734ed9ff6983308b03
Winner Olusola <OLUSOLAWINNER1@GMAIL.COM>
2025-11-06 08:09:02 +0100

    🚢 ship: fix (#18)
    * 🚧 migration fix
    
    * ✨ feat: update readme
    
    * 🛠️ ~ deployment changes

--- packages/trekka-api/src/app.controller.ts
+++ packages/trekka-api/src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { VERSION_NEUTRAL } from '@nestjs/common';
+import { SkipAuth } from './auth/decorator';
 
 @Controller({version: VERSION_NEUTRAL})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @SkipAuth()
   @Get()
   getHello(): string {
     return this.appService.getHello();
